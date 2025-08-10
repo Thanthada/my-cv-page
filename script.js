3a69b81859795583e7d0db303e791e7daf1e6bf0
Thanthada <thanthada_s@cmu.ac.th>
2025-08-10 23:20:17 +0700

    my-cv-page02

---
+++ script.js
@@ -0,0 +1,5 @@
+const contactButton = document.getElementById('contactBtn');
+
+contactButton.addEventListener('click', function() {
+    alert('You can contact me via email: thanthada_s@cmu.ac.th');
+});
